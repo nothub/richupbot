067edd9cd29b148b44029c81bbdb75a41ae9cf97
Florian Hübner <code@hub.lol>
2023-09-16 17:06:43 +0200

    init

---
+++ richup.io.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         richup.io
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Try to take over the world!
+// @author       hub
+// @match        https://richup.io/room/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=richup.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict'
+
+    const app = document.getElementById("app");
+
+    app.
+
+    setInterval(function() {
+        app.childNodes
+            console.log()
+        }, 1000);
+})();
+
+// button text:
+// Roll the dice
+// Roll again
+// End turn
+// Buy for $
