cc1c6edc4199dc00e9396c02694d978836bff48a
Florian Hübner <code@hub.lol>
2023-09-16 17:59:47 +0200

    simple

--- richup.io.user.js
+++ richup.io.user.js
@@ -9,21 +9,81 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict'
+var button_roll_dice
+var button_roll_again
+var button_end_turn
+var button_buy_for_$
 
+function clearButtons() {
+    button_roll_dice = null
+    button_roll_again = null
+    button_end_turn = null
+    button_buy_for_$ = null
+}
+
+function findButtons(node) {
+    const clone = node.cloneNode(false)
+    clone.innerHTML = "" // drop clones children
+    switch (clone.textContent) {
+        case "Roll the dice": {
+            button_roll_dice = findButtonParent(node)
+            break
+        }
+        case "Roll again": {
+            button_roll_again = findButtonParent(node)
+            break
+        }
+        case "End turn": {
+            button_end_turn = findButtonParent(node)
+            break
+        }
+        case "Buy for $": {
+            button_buy_for_$ = findButtonParent(node)
+            break
+        }
+    }
+    node.childNodes.forEach(node => findButtons(node))
+}
+
+function findButtonParent(node) {
+    if (node instanceof HTMLButtonElement) {
+        return node
+    } else {
+        return findButtonParent(node.parentNode)
+    }
+}
+
+function clickElement(element) {
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("click", true, true);
+    element.dispatchEvent(evt)
+}
+
+(function () {
+    'use strict'
     const app = document.getElementById("app");
 
-    app.
+    setInterval(function () {
 
-    setInterval(function() {
-        app.childNodes
-            console.log()
-        }, 1000);
+        clearButtons()
+        findButtons(app)
+
+        if (button_buy_for_$ != null) {
+            console.log("button_buy_for_$")
+            clickElement(button_buy_for_$)
+        }
+        if (button_roll_dice != null) {
+            console.log("button_roll_dice")
+            clickElement(button_roll_dice)
+        }
+        if (button_roll_again != null) {
+            console.log("button_roll_again")
+            clickElement(button_roll_again)
+        }
+        if (button_end_turn != null) {
+            console.log("button_end_turn")
+            clickElement(button_end_turn)
+        }
+
+    }, 1000);
 })();
-
-// button text:
-// Roll the dice
-// Roll again
-// End turn
-// Buy for $
